a3efc12d437e5024a084cf51d72bf7c5b60b3f55
tianhao <tianhaolsk@qq.com>
2021-04-30 17:27:26 +0800

    commit code

---
+++ springboot-web-demo/src/main/java/com/mooc/demo/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.mooc.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Michael on 2018/9/29.
+ */
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    public String home() {
+
+        return "This is SpringBoot Web app";
+
+    }
+
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam String name) {
+
+        return "Hello "+name+"! I'm springboot-web-demo controller!";
+
+    }
+}
